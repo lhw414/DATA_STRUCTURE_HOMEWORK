b6bf2d9ed8a0a05c6b103f4c9295a99d63b181ca
lhw414 <dlgusdn0414@snu.ac.kr>
2023-03-27 20:46:05 +0900

    evaluate 구현 완료

--- HW1/code/BigInteger.java
+++ HW1/code/BigInteger.java
@@ -11,12 +11,11 @@ public class BigInteger
 {
     public static final String QUIT_COMMAND = "quit";
     public static final String MSG_INVALID_INPUT = "Wrong Input";
-  
-    // implement this
-    public static final Pattern EXPRESSION_PATTERN = Pattern.compile("");
+    public static final Pattern EXPRESSION_PATTERN = Pattern.compile("\\s*([-+])?\\s*(\\d+)\\s*([+\\-*])\\s*(?:([-+])?\\s*(\\d+))?\\s*");
 
     public byte[] bytes = new byte[200];
-    public int BigInteger_length;
+    public int BigIntegerLength;
+    public boolean sign = true;
 
     public BigInteger(int num)
     {
@@ -26,7 +25,7 @@ public class BigInteger
             this.bytes[i++] = (byte) (num % 10);
             num /= 10;
         }
-        this.BigInteger_length = i;
+        this.BigIntegerLength = i;
     }
   
     public BigInteger(int[] num1)
@@ -35,7 +34,7 @@ public class BigInteger
         for (int i=0; i<num1.length; i++) {
             bytes[i] = (byte) (num1[i]);
         }
-        this.BigInteger_length = num1.length + 1;
+        this.BigIntegerLength = num1.length + 1;
     }
   
     public BigInteger(String s)
@@ -46,19 +45,31 @@ public class BigInteger
         for (int i=trim_s_length-1; i>=0; i--) {
             bytes[trim_s_length-1-i] = (byte) (trim_s.charAt(i) - '0');
         }
-        this.BigInteger_length = trim_s_length;
+        this.BigIntegerLength = trim_s_length;
 
+    }
+
+    public BigInteger(String s, Boolean sign)
+    {
+        Arrays.fill(bytes, (byte) 0);
+        String trim_s = s.trim();
+        int trim_s_length = trim_s.length();
+        for (int i=trim_s_length-1; i>=0; i--) {
+            bytes[trim_s_length-1-i] = (byte) (trim_s.charAt(i) - '0');
+        }
+        this.BigIntegerLength = trim_s_length;
+        this.sign = sign;
     }
   
     public BigInteger add(BigInteger big)
     {
-        int max_length = (BigInteger_length > big.BigInteger_length) ? BigInteger_length : big.BigInteger_length;
+        int max_length = (this.BigIntegerLength > big.BigIntegerLength) ? this.BigIntegerLength : big.BigIntegerLength;
         int[] result = new int[max_length+1];
         int num_sum, sum;
         int carry = 0;
 
         for (int i=0; i<max_length; i++) {
-            num_sum = bytes[i] + big.bytes[i];
+            num_sum = this.bytes[i] + big.bytes[i] + carry;
             carry = num_sum / 10;
             sum = num_sum % 10;
             result[i] = sum;
@@ -66,15 +77,16 @@ public class BigInteger
         result[max_length] = carry;
 
         BigInteger result_BigInteger = new BigInteger(result);
+        result_BigInteger.BigIntegerLength = max_length + carry;
 
         return result_BigInteger;
     }
   
     public BigInteger subtract(BigInteger big)
     {
-        int max_length = (BigInteger_length > big.BigInteger_length) ? BigInteger_length : big.BigInteger_length;
+        int max_length = (BigIntegerLength > big.BigIntegerLength) ? BigIntegerLength : big.BigIntegerLength;
         int[] result = new int[max_length];
-        int num_sub, sub;
+        int numSub, sub;
         int borrow = 0;
 
         for (int i=0; i<max_length; i++) {
@@ -89,13 +101,20 @@ public class BigInteger
   
     public BigInteger multiply(BigInteger big)
     {
+        int max_length = this.BigIntegerLength + big.BigIntegerLength;
+        int[] result = new int[max_length];
+
+        BigInteger result_BigInteger = new BigInteger(result);
+
+        return result_BigInteger;
     }
   
     @Override
     public String toString()
     {
         StringBuilder sb = new StringBuilder();
-        for (int i=this.BigInteger_length; i>=0; i--) {
+        if(!sign) {sb.append("-");}
+        for (int i=this.BigIntegerLength-1; i>=0; i--) {
             sb.append(bytes[i]);
         }
 
@@ -104,15 +123,51 @@ public class BigInteger
   
     static BigInteger evaluate(String input) throws IllegalArgumentException
     {
-        // implement here
-        // parse input
-        // using regex is allowed
+        //Init value
+        char bigInteger1Sign = '+';
+        String bigIntegerStr1 = "";
+        char operator = '+';
+        char bigInteger2Sign = '+';
+        String bigIntegerStr2 = "";
+
+        //Using regex to matcher
+        Matcher matcher = EXPRESSION_PATTERN.matcher(input);
+
+        //Matching sign, number, operator
+        if (matcher.matches()) {
+            if (matcher.group(1) != null) {
+                bigInteger1Sign = matcher.group(1).charAt(0);
+            }     
+            bigIntegerStr1 = matcher.group(2);
+            operator = matcher.group(3).charAt(0);
+            if (matcher.group(4) != null) {
+                bigInteger2Sign = matcher.group(4).charAt(0);
+            }
+            bigIntegerStr2 = matcher.group(5);
+        }
+
+        BigInteger bigInteger1 = new BigInteger(bigIntegerStr1, (bigInteger1Sign == '+') ? true : false);
+        BigInteger bigInteger2 = new BigInteger(bigIntegerStr2, (bigInteger2Sign == '+') ? true : false);
+        BigInteger result;
+
+        if (operator == '+') {
+            if (bigInteger1Sign == bigInteger2Sign) {
+                result = bigInteger1.add(bigInteger2);
+                result.sign = bigInteger1.sign;
+            } else {
+                result = bigInteger1.subtract(bigInteger2);
+            }
+        } else if (operator == '-') {
+            if (bigInteger1Sign != bigInteger2Sign) {
+                result = bigInteger1.add(bigInteger2);
+            } else {
+                result = bigInteger1.subtract(bigInteger2);
+            }
+        } else {
+            result = bigInteger1.multiply(bigInteger2);
+        }
   
-        // One possible implementation
-        // BigInteger num1 = new BigInteger(arg1);
-        // BigInteger num2 = new BigInteger(arg2);
-        // BigInteger result = num1.add(num2);
-        // return result;
+        return result;
     }
   
     public static void main(String[] args) throws Exception
