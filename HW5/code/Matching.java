a22512733dc7bd8e54e9d633fcb26202346d951b
lhw414 <dlgusdn0414@snu.ac.kr>
2023-05-22 18:30:05 +0900

    Feat : '@'구현완료

--- HW5/code/Matching.java
+++ HW5/code/Matching.java
@@ -5,6 +5,7 @@ public class Matching
 	public static void main(String args[])
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashTable<String, AVLTree<IndexTuple, String>> hashtable = new HashTable<>();
 
 		while (true)
 		{
@@ -14,7 +15,7 @@ public class Matching
 				if (input.compareTo("QUIT") == 0)
 					break;
 
-				command(input);
+				command(input, hashtable);
 			}
 			catch (IOException e)
 			{
@@ -23,12 +24,12 @@ public class Matching
 		}
 	}
 
-	private static void command(String input) throws IOException
+	private static void command(String input, HashTable<String, AVLTree<IndexTuple, String>> hashTable) throws IOException
 	{
 		if (input.charAt(0) == '<') {
-			readFile(input.substring(input.lastIndexOf(" ") + 1));
+			readFile(input.substring(input.lastIndexOf(" ") + 1), hashTable);
 		} else if (input.charAt(0) == '@') {
-
+            printAvlTree(input.substring(input.lastIndexOf(" ") + 1), hashTable);
 		} else if (input.charAt(0) == '?') {
 
 		} else if (input.charAt(0) == '+') {
@@ -40,138 +41,289 @@ public class Matching
 		}
 	}
 
-	private static void readFile(String filepath) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(filepath));
-		String line;
-		while((line = reader.readLine()) != null) {
-			System.out.println(line);
-		}
-
-		reader.close();
+	private static void readFile(String filepath, HashTable<String, AVLTree<IndexTuple, String>> hashTable) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filepath));
+        String line;
+        AVLTree<IndexTuple, String> avlTree; 
+        int i = 1;
+        while((line = reader.readLine()) != null) {
+            for (int j = 0; j < line.length() - 5; j++) {
+                String substring = line.substring(j, j + 6);
+                avlTree = hashTable.get(asciiSumModulo(substring));
+                if (avlTree == null) {
+                    avlTree = new AVLTree(new IndexTuple(i, j+1), substring);
+                    hashTable.put(asciiSumModulo(substring), avlTree);
+                } else {
+                    avlTree.startInsert(new IndexTuple(i, j+1), substring);
+                }
+            }
+            i++;
+        }
+        reader.close();
 	}
+
+    private static int asciiSumModulo(String s) {
+        int sum = 0;
+        for (char c : s.toCharArray()) {
+            sum += (int) c;
+        }
+        return sum % 100;
+    }
+
+    private static void printAvlTree(String hashIndex, HashTable<String, AVLTree<IndexTuple, String>> hashTable) {
+        AVLTree<IndexTuple, String> avlTree; 
+        avlTree = hashTable.get(Integer.parseInt(hashIndex));
+        if (avlTree == null) {
+            System.out.println("EMPTY");
+        } else {
+            avlTree.preOrderPrint();
+        }
+    }
 }
 
-class Node<T extends Comparable<T>, V> {
-	T indexTuple;
-	V value;
-    int height;
-    Node<T, V> left, right;
+class LinkedListNode<T extends Comparable<T>> {
+    T indexTuple;
+    LinkedListNode<T> next;
 
-    public Node(T indexTuple, V value) {
-		this.indexTuple = indexTuple;
+    public LinkedListNode(T indexTuple) {
+        this.indexTuple = indexTuple;
+        this.next = null;
+    }
+}
+
+class LinkedList<T extends Comparable<T>, V extends Comparable<V>> {
+    LinkedListNode<T> head; // dummy head
+    V value;
+    int numitems;
+    int height;
+    LinkedList<T, V> left, right;
+
+    public LinkedList(V value) {
+        this.head = new LinkedListNode<>(null);
+        this.left = null;
+        this.right = null;
         this.value = value;
+        numitems = 0;
         height = 1;
     }
 
-	public Node(T indexTuple, V value, Node<T, V> leftchild, Node<T, V> rightchild, int height) {
-		this.indexTuple = indexTuple;
-		this.value = value;
-		this.left = leftchild;
-		this.right = rightchild;
-		this.height = height;
-	}
+    public LinkedList(T indexTuple, V value) {
+        this.head = new LinkedListNode<>(null);
+        this.head.next = new LinkedListNode<>(indexTuple);
+        this.left = null;
+        this.right = null;
+        this.value = value;
+        numitems = 0;
+        height = 1;
+    }
+
+    public LinkedList(T indexTuple, V value, LinkedList<T, V> left, LinkedList<T, V> right, int heigth) {
+        this.head = new LinkedListNode<T>(null);
+        this.head.next = new LinkedListNode<T>(indexTuple);
+        this.left = left;
+        this.right = right;
+        this.value = value;
+        this.height = heigth;
+        numitems = 1;
+    }
+
+    // Append a new node at the end of the list
+    public void append(T indexTuple) {
+        LinkedListNode<T> newNode = new LinkedListNode<>(indexTuple);
+        LinkedListNode<T> currentNode = this.head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+        numitems += 1;
+    }
+
+    // Insert a new node at a sorted position in the list
+    public void insert(T indexTuple) {
+        LinkedListNode<T> newNode = new LinkedListNode<>(indexTuple);
+        LinkedListNode<T> currentNode = this.head;
+        while (currentNode.next != null && currentNode.next.indexTuple.compareTo(indexTuple) < 0) {
+            currentNode = currentNode.next;
+        }
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+        numitems += 1;
+    }
+
+
+    // Remove a node with the specified indexTuple
+    public void remove(T indexTuple) {
+        LinkedListNode<T> currentNode = this.head;
+        while (currentNode.next != null) {
+            if (currentNode.next.indexTuple.equals(indexTuple)) {
+                currentNode.next = currentNode.next.next;
+                numitems -= 1;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+    }
+
+    // Find a node with the specified indexTuple
+    public LinkedListNode<T> find(T indexTuple) {
+        LinkedListNode<T> currentNode = this.head;
+        while (currentNode.next != null) {
+            if (currentNode.next.indexTuple.equals(indexTuple)) {
+                return currentNode.next;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
 }
 
-class AVLTree<T extends Comparable<T>, V> {
-    private Node<T, V> root;
+class AVLTree<T extends Comparable<T>, V extends Comparable<V>> {
+    private final int LL=1, LR=2, RR=3, RL=4, NO_NEED=0, ILLEGAL=-1;
+    final LinkedList<T, V> NIL = new LinkedList<>(null, null, null, null, 0);
+    private LinkedList<T, V> root;
 
 	public AVLTree() {
-		root = null;
+		root = NIL;
 	}
 
-	int height(Node<T, V> node) {
-        if (node == null)
+    public AVLTree(T indexTuple, V value) {
+        root = new LinkedList(indexTuple, value, NIL, NIL, 1);
+    }
+
+	public int height(LinkedList<T, V> linkedList) {
+        if (linkedList == null)
             return 0;
-        return node.height;
+        return linkedList.height;
+    }
+
+    public LinkedList<T, V> getNIL() {
+        return NIL;
+    }
+
+    public LinkedList<T, V> getRoot() {
+        return root;
     }
 
 	public void preOrderPrint() {
-		if (root.left == null && root.right == null) {
+		if (root == null || root == NIL) {
 			System.out.println("EMPTY");
 		} else {
-			System.out.print(this.root.indexTuple);
-			preOrderPrint(root.left);
-			preOrderPrint(root.right);
+			System.out.print(this.root.value);
+            if (root.left != null && root.left != NIL) {
+                preOrderPrint(root.left);
+            }
+			if (root.right != null && root.right != NIL) {
+                preOrderPrint(root.right);
+            }
+            System.out.println("");
 		}
 	}
 
-	public void preOrderPrint(Node<T, V> currentNode) {
-		if (currentNode != null) {
-			System.out.print(currentNode.indexTuple);
-			preOrderPrint(root.left);
-			preOrderPrint(root.right);
-		}
+	public void preOrderPrint(LinkedList<T, V> currentList) {
+		System.out.print(" " + currentList.value);
+        if (currentList.left != null && currentList.left != NIL) {
+            preOrderPrint(currentList.left);
+        }
+		if (currentList.right != null && currentList.right != NIL) {
+            preOrderPrint(currentList.right);
+        }
 	}
 
-    Node<T, V> rotateRight(Node<T, V> y) {
-        Node<T, V> x = y.left;
-        Node<T, V> T2 = x.right;
-
-        x.right = y;
-        y.left = T2;
-
-        y.height = Math.max(height(y.left), height(y.right)) + 1;
-        x.height = Math.max(height(x.left), height(x.right)) + 1;
-
-        return x;
+    private int needBalance(LinkedList<T, V> linkedlist) {
+        int type = ILLEGAL;
+        if (linkedlist.left.height+2 <= linkedlist.right.height) {
+            if (linkedlist.right.left.height <= linkedlist.right.right.height) {
+                type = RR;
+            } else {
+                type = RL;
+                }
+        } else if (linkedlist.left.height >= linkedlist.right.height + 2) {
+            if (linkedlist.left.left.height >= linkedlist.left.right.height) {
+                type = LL;
+            } else {
+                type = LR;
+            }
+        } else {
+            type = NO_NEED;
+        }
+        return type;
     }
 
-    Node<T, V> rotateLeft(Node<T, V> x) {
-        Node<T, V> y = x.right;
-        Node<T, V> T2 = y.left;
+    private LinkedList<T, V> balanceAVL(LinkedList<T, V> linkedList, int type) {
+        LinkedList<T, V> returnList = NIL;
+        switch (type) {
+            case LL:
+                returnList = rotateRight(linkedList);
+                break;
+            case LR:
+                linkedList.left = rotateLeft(linkedList.left);
+                returnList = rotateRight(linkedList);
+                break;
+            case RR:
+                returnList = rotateLeft(linkedList);
+                break;
+            case RL:
+                linkedList.right = rotateRight(linkedList);
+                returnList = rotateLeft(linkedList);
+                break;
+        }
 
-        y.left = x;
-        x.right = T2;
-
-        x.height = Math.max(height(x.left), height(x.right)) + 1;
-        y.height = Math.max(height(y.left), height(y.right)) + 1;
-
-        return y;
+        return returnList;
     }
 
-    int getBalance(Node<T, V> node) {
-        if (node == null)
-            return 0;
-        return height(node.left) - height(node.right);
+    LinkedList<T, V> rotateRight(LinkedList<T, V> t) {
+        LinkedList<T, V> LChild = t.left;
+        if (LChild == NIL || LChild == null) {
+            return NIL;
+        }
+        LinkedList<T, V> LRChild = LChild.right;
+        LChild.right = t;
+        t.left = LRChild;
+        t.height = 1 + Math.max(t.left.height, t.right.height);
+        LChild.height = 1 + Math.max(LChild.left.height, LChild.right.height);
+        return LChild;
+    }
+    
+    LinkedList<T, V> rotateLeft(LinkedList<T, V> t) {
+        LinkedList<T, V> RChild = t.right;
+        if (RChild == NIL || RChild == null) {
+            return NIL;
+        }
+        LinkedList<T, V> RLChild = RChild.left;
+        RChild.left = t;
+        t.right = RLChild;
+        t.height = 1 + Math.max(t.left.height, t.right.height);
+        RChild.height = 1 + Math.max(RChild.left.height, RChild.right.height);
+        return RChild;
+    }
+    
+    void startInsert(T indexTuple, V value) {
+        root = insert(root, indexTuple, value);
     }
 
-    Node<T, V> insert(Node<T, V> node, T indexTuple, V value) { // 수정: 인자 변경
-        if (node == null)
-            return new Node<>(indexTuple, value);
-
-        if (indexTuple.compareTo(node.indexTuple) < 0) { // 수정: indexTuple 비교
-            node.left = insert(node.left, indexTuple, value);
-        } else if (indexTuple.compareTo(node.indexTuple) > 0) { // 수정: indexTuple 비교
-            node.right = insert(node.right, indexTuple, value);
-        } else { // 수정: indexTuple이 같을 때는 중복을 허용하지 않거나 다른 처리를 수행할 수 있습니다.
-            return node;
+    LinkedList<T, V> insert(LinkedList<T, V> linkedList, T indexTuple, V value) {
+        if (linkedList == NIL || linkedList == null) {
+            linkedList =  new LinkedList<>(indexTuple, value, NIL, NIL, 1);
+        } else if (value.compareTo(linkedList.value) < 0) {
+            linkedList.left = insert(linkedList.left, indexTuple, value);
+            linkedList.height = 1 + Math.max(linkedList.left.height, linkedList.right.height);
+            int type = needBalance(linkedList);
+            if (type != NO_NEED) {
+                linkedList = balanceAVL(linkedList, type);
+            }
+        } else if (value.compareTo(linkedList.value) > 0) {
+            linkedList.right = insert(linkedList.right, indexTuple, value);
+            linkedList.height = 1 + Math.max(linkedList.left.height, linkedList.right.height);
+            int type = needBalance(linkedList);
+            if (type != NO_NEED) {
+                linkedList = balanceAVL(linkedList, type);
+            }
+        } else {
+            linkedList.insert(indexTuple);
         }
-
-        node.height = 1 + Math.max(height(node.left), height(node.right));
-
-        int balance = getBalance(node);
-
-        if (balance > 1 && indexTuple.compareTo(node.left.indexTuple) < 0) { // 수정: indexTuple 비교
-            return rotateRight(node);
-        }
-
-        if (balance < -1 && indexTuple.compareTo(node.right.indexTuple) > 0) { // 수정: indexTuple 비교
-            return rotateLeft(node);
-        }
-
-        if (balance > 1 && indexTuple.compareTo(node.left.indexTuple) > 0) { // 수정: indexTuple 비교
-            node.left = rotateLeft(node.left);
-            return rotateRight(node);
-        }
-
-        if (balance < -1 && indexTuple.compareTo(node.right.indexTuple) < 0) { // 수정: indexTuple 비교
-            node.right = rotateRight(node.right);
-            return rotateLeft(node);
-        }
-
-        return node;
+        return linkedList;
     }
-
 }
 
 class HashTable<K, V> {
@@ -183,21 +335,43 @@ class HashTable<K, V> {
 		this.hashTable = (V[]) new Object[DEFAULT_SIZE];
 	}
 
-	public int hash(K x) {
-		return x.hashCode() % DEFAULT_SIZE;
-	}
-
-	public void put(K key, V value) {
-        int index = hash(key);
-		if (hashTable[index] == null) {
-
-		} else {
-			
-		}
+	public void put(int index, V value) {
+        hashTable[index] = value;
     }
 
 	public V get(int index) {
 		return hashTable[index];
 	}
     
+}
+
+class IndexTuple implements Comparable<IndexTuple> {
+    private int index1;
+    private int index2;
+
+    public IndexTuple(int index1, int index2) {
+        this.index1 = index1;
+        this.index2 = index2;
+    }
+
+    public int getIndex1() {
+        return index1;
+    }
+
+    public int getIndex2() {
+        return index2;
+    }
+
+    @Override
+    public int compareTo(IndexTuple other) {
+        if (this.index1 != other.index1) {
+            return Integer.compare(this.index1, other.index1);
+        }
+        return Integer.compare(this.index2, other.index2);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + index1 + ", " + index2 + ")";
+    }
 }
