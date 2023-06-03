b71ccd98def473a0b12ff505db1f0e7e62e75da9
lhw414 <dlgusdn0414@snu.ac.kr>
2023-06-03 19:14:55 +0900

    Feat : file에서 지하철 정보 읽어오도록 변경

--- HW6/code/Subway.java
+++ HW6/code/Subway.java
@@ -6,77 +6,75 @@ public class Subway {
         String input;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Graph graph = new Graph();
-        Boolean flag = true;
         try {
+            graph = readFile(args[0], graph);
+
             while(true) {
-                while(flag) { // read station info
-                    input = br.readLine();
-                    if (input.equals("QUIT")) {
-                        flag = false;
-                        break;
-                    }
-                    if (input.isEmpty()) {
-                        break; // move next read mode
-                    }
-                    String[] inputList = input.split(" ");
-                    Station newStation = new Station(inputList[0], inputList[1], inputList[2]);
-                    if (graph.isSameNameStationExist(inputList[1])) {
-                        graph.addDuplicateStation(newStation);
-                    } else {
-                        graph.addStation(newStation);
-                    }
+                input = br.readLine();
+                if (input.equals("QUIT")) {
+                    break;
                 }
-                while(flag) {
-                    input = br.readLine();
-                    if (input.equals("QUIT")) {
-                        flag = false;
-                        break;
-                    }
-                    if (input.isEmpty()) {
-                        break; // move next read mode
-                    }
-                    String[] inputList = input.split(" ");
-                    Edge newEdge = new Edge(inputList[0], inputList[1], Integer.parseInt(inputList[2]));
-                    graph.addEdge(newEdge);
-                }
-                while(flag) {
-                    input = br.readLine();
-                    if (input.equals("QUIT")) {
-                        flag = false;
-                        break;
-                    }
-                    if (input.isEmpty()) {
-                        break; // move next read mode
-                    }
-                    String[] inputList = input.split(" ");
-                    graph.updateTransferLine(inputList[0], Integer.parseInt(inputList[1]));
-                }
-                while(flag) {
-                    input = br.readLine();
-                    if (input.equals("QUIT")) {
-                        break;
-                    }
-                    String[] inputList = input.split(" ");
-                    List<Station> startStationList = graph.getSameNameList(inputList[0]);
-                    List<Station> endStationList = graph.getSameNameList(inputList[1]);
-                    Path ansPath = new Path(new LinkedList<String>(), Integer.MAX_VALUE);
-                    for(int i=0;i<startStationList.size();i++) {
-                        for (int j=0;j<endStationList.size();j++) {
-                            Path currpath = graph.findShortestPath(startStationList.get(i).getId(), endStationList.get(j).getId());
-                            if (ansPath.compareTo(currpath) < 0) {
-                                ansPath = currpath;
-                            }
+                String[] inputList = input.split(" ");
+                List<Station> startStationList = graph.getSameNameList(inputList[0]);
+                List<Station> endStationList = graph.getSameNameList(inputList[1]);
+                Path ansPath = new Path(new LinkedList<String>(), Integer.MAX_VALUE);
+                for(int i=0;i<startStationList.size();i++) {
+                    for (int j=0;j<endStationList.size();j++) {
+                        Path currpath = graph.findShortestPath(startStationList.get(i).getId(), endStationList.get(j).getId());
+                        if (ansPath.compareTo(currpath) < 0) {
+                            ansPath = currpath;
                         }
                     }
-                    System.out.println(ansPath.toString());
-                    System.out.println(ansPath.getTotalTravelTime());
                 }
-                break;
+                System.out.println(ansPath.toString());
+                System.out.println(ansPath.getTotalTravelTime());
             }
         } catch (IOException e) {
             System.out.println(e.toString());
         }
     }
 
+    public static Graph readFile(String filepath, Graph graph) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), "UTF-8"));
+        String input;
+        try{
+            while(true) { // read station info
+                input = br.readLine();
+                if (input == null || input.isEmpty()) {
+                    break; // move next read mode
+                }
+                String[] inputList = input.split(" ");
+                Station newStation = new Station(inputList[0], inputList[1], inputList[2]);
+                if (graph.isSameNameStationExist(inputList[1])) {
+                    graph.addDuplicateStation(newStation);
+                } else {
+                    graph.addStation(newStation);
+                }
+            }
+            while(true) {
+                input = br.readLine();
+                if (input == null || input.isEmpty()) {
+                    break; // move next read mode
+                }
+                String[] inputList = input.split(" ");
+                Edge newEdge = new Edge(inputList[0], inputList[1], Integer.parseInt(inputList[2]));
+                graph.addEdge(newEdge);
+            }
+            while(true) {
+                input = br.readLine();
+                if (input == null || input.isEmpty()) {
+                    break; // move next read mode
+                }
+                String[] inputList = input.split(" ");
+                graph.updateTransferLine(inputList[0], Integer.parseInt(inputList[1]));
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        return graph;
+    }
 
 }
