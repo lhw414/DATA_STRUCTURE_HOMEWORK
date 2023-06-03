c63f152b5d35979fd04de55f05d8bae7459be582
lhw414 <dlgusdn0414@snu.ac.kr>
2023-06-03 19:50:31 +0900

    Fix : 한글 입력 오류 해결

--- HW6/code/Subway.java
+++ HW6/code/Subway.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Subway {
     public static void main(String args[]) throws IOException {
         String input;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
         Graph graph = new Graph();
         try {
             graph = readFile(args[0], graph);
